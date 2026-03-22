7229a804396c7fcc39ebff7fa12f50325aba9e08
Freeze <freezeyaboy@gmail.com>
2026-03-21 18:12:03 -0700

    Refactor CPU watcher to use time.Duration for polling

--- cpuwatcher/cpuwatcher.go
+++ cpuwatcher/cpuwatcher.go
@@ -1,19 +1,28 @@
 package cpuwatcher
 
-import "fmt"
+import (
+	"fmt"
+	"time"
 
-var pollingRate float64 = 1.0 // Default polling rate in seconds
+	"github.com/shirou/gopsutil/v3/cpu"
+)
 
-func WatchCPU() { 
-	
-	var cpuUsage float64
+var cpuPollingRate time.Duration = 1 * time.Second
 
-	cpuUsage = 75.5 // Example CPU usage value
-	fmt.Printf("Watching CPU usage - Current usage: %v - polling rate: %v seconds", cpuUsage, pollingRate)
+func WatchCPU() {
+
+	for {
+		cpuPercent, err := cpu.Percent(cpuPollingRate, false)
+		if err != nil {
+			fmt.Println("Error fetching CPU info:", err)
+			return
+		}
+		fmt.Printf("Watching CPU usage - Current usage: %.2f%% - polling rate: %v seconds\n", cpuPercent[0], cpuPollingRate)
+	}
 }
 
 func SetPollingRate(rate float64) {
 	// In a real implementation, you would validate the input and update the polling rate accordingly.
-	pollingRate = rate
+	cpuPollingRate = time.Duration(rate * float64(time.Second))
 	fmt.Printf("Setting CPU watcher polling rate to %v seconds\n", rate)
 }
