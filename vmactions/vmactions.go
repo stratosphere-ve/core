1fa828c392ad163355325e5c60f04cdb0fdae98f
Freeze <freezeyaboy@gmail.com>
2026-03-22 22:58:23 -0700

    Update vmactions.go

--- vmactions/vmactions.go
+++ vmactions/vmactions.go
@@ -1,8 +1,41 @@
 package vmactions
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func VMCreate() {
-	fmt.Println("Creating VM...")
-	// Placeholder for VM creation logic
-}
+	os.MkdirAll("vms", os.ModePerm)
 
+	vmname := ""
+
+	files, err := os.ReadDir("vms")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Enter VM name: ")
+	fmt.Scanln(&vmname)
+
+	for _, f := range files {
+		name := strings.TrimSuffix(f.Name(), ".json")
+		if name == vmname {
+			fmt.Printf("A VM with that name (%v) already exists. Please choose a different name.", vmname)
+			return
+		}
+	}
+
+	fmt.Printf("Creating VM with name: %v", vmname)
+
+	file, err := os.Create(fmt.Sprintf("vms/%v.json", vmname))
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Println("Successfully created VM!")
+}
